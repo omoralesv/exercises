0961cbb24b6b6b66b2f9c3339adf493d5e705c84
Omar Morales <omarmv101@yahoo.com>
2023-02-01 19:54:00 -0800

    fixing homepage

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDom.createRoot(document.getElementById('root'))
 
 root.render (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App/>
     </BrowserRouter>
 )
